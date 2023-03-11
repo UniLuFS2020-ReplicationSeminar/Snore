ce842073baee774e788110218687309598d9dc57
Marius Imholz <mare.imholz@sunrise.ch>
2023-03-11 11:09:54 +0100

    adds data modeling r script

--- Scripts/data_import.R
+++ Scripts/data_import.R
@@ -7,7 +7,5 @@ sleep <- read.csv("Sleep_Efficiency.csv")
 
 #select dependent and independent variables
 sl1 <- sleep %>% 
-  select(c(ID, Sleep.efficiency, Caffeine.consumption, Alcohol.consumption, Smoking.status, 
+  select(c(ID, Age,Sleep.efficiency, Caffeine.consumption, Alcohol.consumption, Smoking.status, 
            Exercise.frequency))
-
-
---
+++ Scripts/data_modeling.R
@@ -0,0 +1,14 @@
+#Data modeling
+install.packages("BBmisc")
+install.packages("stargazer")
+library(BBmisc)
+library(stargazer)
+
+slsc <- normalize(x = sl1[,3:7])
+summary(slsc)
+
+model1<-lm(slsc$Sleep.efficiency~slsc$Caffeine.consumption+slsc$Alcohol.consumption+slsc$Smoking.status)
+summary(model1)
+summary(model1)$coefficient
+confint(model1)
+stargazer(model1, type='text')
